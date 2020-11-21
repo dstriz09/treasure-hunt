0315ff4df13653f497036bc1acef9db507069c34
Dillin Strizzi <dillin@MacBook-Air-2.local>
2020-11-21 13:14:15 -0800

    Added Gameboard View

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Board from "./Board";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Treasure-Hunt</h1>
+      <Board />
     </div>
   );
 }
