0b7ba76481732d0f38c586e3ecee5eb9b46f50ea
Brandon Morelli <bmorelli25@gmail.com>
2020-11-27 16:05:36 -0800

    one more

--- src/CardContext.js
+++ src/CardContext.js
@@ -25,7 +25,6 @@ const CardContextProvider = (props) => {
     treasureDeck: NewTreasureDeck(),
     currentRound: 1, // seven rounds per phase
     currentPhase: 1, // four phases per game
-    currentTreasure: 0,
     numPlayers: null,
     playerOne: { current: [], complete: [] },
     playerTwo: { current: [], complete: [] },
