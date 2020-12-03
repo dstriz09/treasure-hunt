bad3d0e599c5e095fae2448141703d27c8fdc7c2
Brandon Morelli <bmorelli25@gmail.com>
2020-12-02 20:58:18 -0800

    more

---
+++ src/helpers/GameSetup.js
@@ -0,0 +1,9 @@
+// export const CreatePlayers = (players) => {
+//   let temp = [];
+//   for (let i = 0; i < players; i++) {
+//     temp.push(<Players playerid={i} key={i} />);
+//   }
+//   return temp;
+// };
+
+export const AssignTreasures = () => {};
