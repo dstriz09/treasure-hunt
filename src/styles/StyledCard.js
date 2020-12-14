2cf5d62d61bab4d8e5f7075f419ad6e97f73c792
dstriz09 <37376002+dstriz09@users.noreply.github.com>
2020-12-14 12:40:32 -0800

    added square images and styles (#39)
    * added square images and styles
    
    * removed redundencies
    
    * Update src/styles/StyledCard.js
    
    Co-authored-by: Brandon Morelli <bmorelli25@gmail.com>
    
    * Update src/components/Square.js
    
    Co-authored-by: Brandon Morelli <bmorelli25@gmail.com>
    
    * Update src/styles/StyledSquare.js
    
    Co-authored-by: Brandon Morelli <bmorelli25@gmail.com>
    
    * color variables in treasureCards
    
    * Update src/components/Grid.js
    
    Co-authored-by: Brandon Morelli <bmorelli25@gmail.com>
    
    Co-authored-by: Brandon Morelli <bmorelli25@gmail.com>

--- src/styles/StyledCard.js
+++ src/styles/StyledCard.js
@@ -10,4 +10,6 @@ export const StyledCard = styled.div`
   box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
   display: flex;
   flex-direction: column;
+  width: 142px;
+  height: 219px;
 `;
