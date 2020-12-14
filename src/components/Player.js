3b77972ec376b0cdee5a9b081135de8d74d681e2
Brandon Morelli <bmorelli25@gmail.com>
2020-12-13 16:10:24 -0800

    small cleanup

--- src/components/Player.js
+++ src/components/Player.js
@@ -7,13 +7,11 @@ import { StyledPlayer, StyledPlayerName } from "../styles/StyledPlayer";
 function Player({ name, cards }) {
   return (
     <StyledPlayer>
-      <StyledPlayerName>
-        <div>{name}</div>
-      </StyledPlayerName>
+      <StyledPlayerName>{name}</StyledPlayerName>
+      <Score />
       {cards.map((card, x) => (
         <Card key={x} card={card} />
       ))}
-      <Score />
     </StyledPlayer>
   );
 }
